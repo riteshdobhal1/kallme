39229bb4e7e84db0539531548aebbc0e0bb33158
Ashwin Mridul <ashwinmridul@Ashwins-MacBook-Pro.local>
2017-07-12 20:05:36 +0530

    REST API for fetching search results

--- apps/restserver/app/category/models.py
+++ apps/restserver/app/category/models.py
@@ -4,7 +4,19 @@ from db import *
 import pymysql
 
 def searchcategory(data):
-	searchdata = data.get('search')
+    searchdata = data.get('search').lower()
+
+    if(len(searchdata) == 0):
+        return []
+
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    qry = "SELECT " + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[0] + " as id, " + tbl_category_dtls.get('name') + "." + tbl_category_dtls.get('columns')[1] + " as category, " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[2] + " as sub_category, " + tbl_city_dtls.get('name') + "." + tbl_city_dtls.get('columns')[1] + " as city, " + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[4] + " as content, " + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[5] + " as image_url FROM " + tbl_category_dtls.get('name') + ", " + tbl_sub_category_dtls.get('name') + ", " + tbl_city_dtls.get('name') + ", " + tbl_customer_data_dtls.get('name') + " WHERE " + tbl_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[0] + "=" + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[2] + " AND " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[0] + "=" + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[3] + " AND " + tbl_city_dtls.get('name') + "." + tbl_city_dtls.get('columns')[0] + "=" + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[1] + " AND " + tbl_category_dtls.get('name') + "." + tbl_category_dtls.get('columns')[2] + "=1 AND " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[3] + "=1 AND LOWER(" + tbl_customer_data_dtls.get('name') + "." + tbl_customer_data_dtls.get('columns')[4] + ") LIKE '%" + searchdata + "%'"
+
+    cursor.execute(qry)
+    results = cursor.fetchall()
+    return results
 
 def listcategory():
 	conn = mysql.connect()
