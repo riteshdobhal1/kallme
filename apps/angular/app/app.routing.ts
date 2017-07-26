0f615267a96154df88f8ef0fe0fa16de9c1580d1
Ashwin Mridul <ashwinmridul@Ashwins-MacBook-Pro.local>
2017-07-26 08:55:31 +0530

    Display ony sub categories in filters + Removing jquery dependency for toggling login/signup

--- apps/angular/app/app.routing.ts
+++ apps/angular/app/app.routing.ts
@@ -1,6 +1,5 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-const appRoutes: Routes = [
-];
+import {ModuleWithProviders}  from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+const appRoutes: Routes = [];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
