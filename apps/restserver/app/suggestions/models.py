ff61bb77ee5d95105eed511a029fd97add5c501e
Ritesh Dobhal <loveedoval@gmail.com>
2017-08-02 09:22:10 -0700

    Adding CSS changes and UI display for category search results

--- apps/restserver/app/suggestions/models.py
+++ apps/restserver/app/suggestions/models.py
@@ -9,8 +9,6 @@ def listSuggestions():
 
     qry = "SELECT " + tbl_suggestion_dtls.get('name') + "." + tbl_suggestion_dtls.get('columns')[0] + " as id, CONCAT('in ', " + tbl_category_dtls.get('name') + "." + tbl_category_dtls.get('columns')[1] + ", '/', " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[2] + ") as description, " + tbl_suggestion_dtls.get('name') + "." + tbl_suggestion_dtls.get('columns')[1] + " as title, " + tbl_suggestion_dtls.get('name') + "." + tbl_suggestion_dtls.get('columns')[4] + " as keywords FROM " + tbl_category_dtls.get('name') + ", " + tbl_sub_category_dtls.get('name') + ", " + tbl_suggestion_dtls.get('name') + " WHERE " + tbl_category_dtls.get('name') + "." + tbl_category_dtls.get('columns')[0] + "=" + tbl_suggestion_dtls.get('name') + "." + tbl_suggestion_dtls.get('columns')[2] + " AND " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[0] + "=" + tbl_suggestion_dtls.get('name') + "." + tbl_suggestion_dtls.get('columns')[3]  + " AND " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[1] + "=" + tbl_category_dtls.get('name') + "." + tbl_category_dtls.get('columns')[0] + " AND " + tbl_category_dtls.get('name') + "." + tbl_category_dtls.get('columns')[2] + "=1 AND " + tbl_sub_category_dtls.get('name') + "." + tbl_sub_category_dtls.get('columns')[3] + "=1"
 
-    print qry
-
     cursor.execute(qry)
     results = cursor.fetchall()
     return results
--- apps/restserver/run.py
+++ apps/restserver/run.py
@@ -1,15 +1,15 @@
 from app import app
-import logging
-from logging.handlers import RotatingFileHandler
+#import logging
+#from logging.handlers import RotatingFileHandler
 
 if __name__ == '__main__':
-    logHandler = RotatingFileHandler('logs/info.log', maxBytes=1000, backupCount=1)
+ #   logHandler = RotatingFileHandler('logs/info.log', maxBytes=1000, backupCount=1)
     
     # set the log handler level
-    logHandler.setLevel(logging.DEBUG)
+  #  logHandler.setLevel(logging.DEBUG)
 
     # set the app logger level
-    app.logger.setLevel(logging.DEBUG)
+   # app.logger.setLevel(logging.DEBUG)
 
-    app.logger.addHandler(logHandler)    
+    #app.logger.addHandler(logHandler)    
     app.run(host='0.0.0.0',debug=app.config["DEBUG"]) 
